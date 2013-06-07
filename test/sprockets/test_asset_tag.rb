9886f25590d1d5d1eca248f3316058ffb5624adb
Sumeet Singh <ortuna@gmail.com>
2013-06-07 13:43:37 -0400

    Better test setup I think

---
+++ test/sprockets/test_asset_tag.rb
@@ -0,0 +1,27 @@
+require File.expand_path(File.dirname(__FILE__) + '/extra/helper')
+
+describe 'Asset tags' do
+
+  context 'javascript_include_tag' do
+    let(:app) { rack_app }
+
+    it 'can use the default javascript_include_tag to resolve JS asset' do
+      skip
+      assets_location = File.expand_path(File.dirname(__FILE__) + '/fixtures/assets_app/assets/javascripts')
+      mock_app do
+        register Padrino::Assets
+        register Padrino::Helpers
+        configure_assets do |assets| 
+          assets.pipeline = Padrino::Assets::Sprockets
+          assets.paths    = [assets_location]
+        end
+        get('/') { render :erb, "<%= javascript_include_tag 'app.js' %>" }
+      end
+
+      get '/'
+      assert_equal '/assets/javascripts/app.js', last_response.body
+    end
+
+  end
+end
+
---
+++ test/sprockets/test_assets.rb
@@ -0,0 +1,40 @@
+require File.expand_path(File.dirname(__FILE__) + '/extra/helper')
+
+describe Padrino::Assets do
+  let(:app) { AssetsAppSprockets }
+
+  context 'for application behavior' do
+    it 'knows that assets should be served' do
+      assert_equal app.serve_assets?, true
+    end
+
+    it 'detects the root location of the running app' do
+      app_path = File.expand_path(File.dirname(__FILE__) + '/fixtures/sprockets_app')
+      assert_equal app_path, app.root
+    end
+  end
+
+  context 'general options' do
+    let(:app) { rack_app }
+    
+    before do
+     @assets_location = File.expand_path(File.dirname(__FILE__) + '/fixtures/sprockets_app/assets/javascripts')
+    end
+
+    it 'can set a general prefix for all asset types' do
+      assets_location = @assets_location
+      mock_app do
+        register Padrino::Assets
+        configure_assets do |assets|
+          assets.pipeline = Padrino::Assets::Sprockets
+          assets.paths    = [assets_location]
+          assets.prefix   = '/trunk'
+        end
+      end
+
+      get '/trunk/assets/javascripts/app.js'
+      assert_equal 200, last_response.status
+    end
+  end
+
+end#describe
