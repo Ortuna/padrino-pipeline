50d10579669d2aa9698882a66822d8782a08defa
Sumeet Singh <ortuna@gmail.com>
2013-06-08 16:00:32 -0400

    Added test for non-default location packs

--- test/asset_pack/test_packages.rb
+++ test/asset_pack/test_packages.rb
@@ -4,16 +4,35 @@ require File.expand_path(File.dirname(__FILE__) + '/../fixtures/asset_pack_app/a
 describe 'AssetPack Packages' do
   let(:app) { rack_app }
 
-  it 'can serve a non-default asset pack' do
-    mock_app do
-      register Padrino::Assets
-      configure_assets do |config|
-        config.pipeline   = Padrino::Assets::AssetPack
-        config.packages << [:js, :application, '/assets/javascripts/application.js', ['/assets/javascripts/*.js']]
+  context 'for javascripts' do
+    it 'can serve an asset pack' do
+      mock_app do
+        register Padrino::Assets
+        configure_assets do |config|
+          config.pipeline   = Padrino::Assets::AssetPack
+          config.packages << [:js, :application, '/assets/javascripts/application.js', ['/assets/javascripts/*.js']]
+        end
       end
+      get '/assets/javascripts/application.js' 
+      assert_equal 200, last_response.status
     end
-    get '/assets/javascripts/application.js' 
-    assert_equal 200, last_response.status
+
+    it 'can serve an asset pack from a non-standard location' do
+      mock_app do
+        register Padrino::Assets
+        configure_assets do |config|
+          config.pipeline   = Padrino::Assets::AssetPack
+          config.js_prefix  = '/meow/javascripts'
+          config.packages << [:js, :application, '/meow/javascripts/application.js', ['/assets/javascripts/*.js']]
+        end
+      end
+      get '/meow/javascripts/application.js' 
+      assert_equal 200, last_response.status
+    end
   end
 
+  context 'for stylesheets' do
+
+
+  end
 end
