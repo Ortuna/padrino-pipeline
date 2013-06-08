75db79ab334db8d291460a8af1650da26aa2f99c
Sumeet Singh <ortuna@gmail.com>
2013-06-08 16:31:00 -0400

    refactor

--- lib/padrino-assets/pipelines/asset_pack.rb
+++ lib/padrino-assets/pipelines/asset_pack.rb
@@ -10,6 +10,26 @@ module Padrino
         setup_pipeline
       end
 
+      def js_prefix
+        (@config.prefix || '') + (@config.js_prefix  || '/assets/javascripts')
+      end
+
+      def css_prefix
+        (@config.prefix || '') + (@config.css_prefix || '/assets/stylesheets')
+      end
+
+      def js_assets
+        @config.js_assets || 'assets/javascripts'
+      end
+
+      def css_assets
+        @config.css_assets || 'assets/stylesheets'
+      end
+
+      def packages
+        @config.packages  || []
+      end
+
       private
       def setup_enviroment
         @app.set :serve_assets, true
@@ -17,20 +37,15 @@ module Padrino
       end
 
       def setup_pipeline
-        js_prefix  = (@config.prefix || '') + (@config.js_prefix  || '/assets/javascripts')
-        css_prefix = (@config.prefix || '') + (@config.css_prefix || '/assets/stylesheets')
-
-        js_assets   = @config.js_assets || 'assets/javascripts'
-        css_assets  = @config.css_assets || 'assets/stylesheets'
-        packages    = @config.packages  || []
+        js_prefix, css_prefix   = self.js_prefix, self.css_prefix
+        js_assets, css_assets   = self.js_assets, self.css_assets
+        packages                = self.packages
 
         @app.assets {
           serve js_prefix, :from => js_assets
           serve css_prefix,:from => css_assets
 
-          packages.each do |package|
-            send(package.shift, *package) 
-          end
+          packages.each { |package| send(package.shift, *package) } 
 
           js_compression  :uglify
           css_compression :simple
