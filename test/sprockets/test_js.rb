1b37c6110e18d04bc8f9b1638bb4d139cbed7b4d
Sumeet Singh <ortuna@gmail.com>
2013-06-09 20:13:08 -0400

    removed old tests

--- test/sprockets/test_js.rb
+++ test/sprockets/test_js.rb
@@ -4,26 +4,11 @@ require File.expand_path(File.dirname(__FILE__) + '/../fixtures/sprockets_app/sp
 
 describe 'Javascripts' do
   let(:app) { AssetsAppSprockets }
-  context 'for javascript assets' do
-    it 'can retrieve a js asset by file name' do
-      get '/assets/javascripts/unrequired.js'
-      assert_match 'var unrequired;', last_response.body
+  context 'for //= require' do
+    it 'picks up require statements' do
+      get '/assets/javascripts/app.js'
+      assert_match 'var in_second_file;', last_response.body
     end
-
-    it 'shows a 404 for unkown assets' do
-      get '/assets/javascripts/xyz.js'
-      assert_not_equal 200, last_response.status
-    end
-
-    context 'for //= require' do
-      it 'picks up require statements' do
-        get '/assets/javascripts/app.js'
-        assert_match 'var in_second_file;', last_response.body
-      end
-    end
-
-    context 'for custom options' do
-    end#context
-    
-  end#context
+  end
+ 
 end#describe
