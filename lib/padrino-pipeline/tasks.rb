30cc853efee4f2b01a05a934041f53f7052c0461
Sumeet Singh <ortuna@gmail.com>
2013-07-02 22:46:09 -0400

    Rescue from load error

--- lib/padrino-pipeline/tasks.rb
+++ lib/padrino-pipeline/tasks.rb
@@ -1,6 +1,9 @@
-require 'padrino-core/tasks'
-##
-# Allows tasks to be added to the client Padrino application
-if defined? Padrino::Tasks
-  Padrino::Tasks.files.concat(Dir["#{File.dirname(__FILE__)}/tasks/**/*.rb"])
+begin
+  require 'padrino-core/tasks'
+  ##
+  # Allows tasks to be added to the client Padrino application
+  if defined? Padrino::Tasks
+    Padrino::Tasks.files.concat(Dir["#{File.dirname(__FILE__)}/tasks/**/*.rb"])
+  end
+rescue LoadError
 end
